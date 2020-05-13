5309a8e0c3b31e015b328b346bf8e3b59e972083
Agata <calkapotrojkacie@gmail.com>
2020-05-13 09:43:31 +0200

    Changed the bug, the program is working fine now

--- Program.cs
+++ Program.cs
@@ -9,20 +9,23 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             double cS=690, cL=800, roS=7500, roL=7000, L=270000, tempKrzep=1773, u0=1813,uI=323,lambdaL=104,lambdaS=240;
-            int lbIteracji = 150;
+            int lbIteracji = 300;
             double a,stalaA,stalaSigma;
-            double entalpiaL = 10453590000, entalpiaS = 8563590000;
-            double b = 0.08,tGwiazdka=10;
+            double entalpiaL = 11200275000, entalpiaS = 9175275000;
+            double b = 0.08,tGwiazdka=100;
             double alfa=0.5;
+            
 
             //double[] temp0= { 50,50,50,50,50}; 
             //int n = temp0.Length;
 
-            int n = 10;
+            int n = 100;
             double[] temp0 = new double[n];
+            double dT = (tempKrzep-uI) / (n - 1);
 
-            for(int i=0;i<n;i++)
+            for (int i=0;i<n;i++)
             {
+                //temp0[i] = tempKrzep-i*dT;
                 temp0[i] = uI;
             }
             temp0[0] = tempKrzep;
@@ -36,7 +39,7 @@ namespace ConsoleApp1
             double[,] tabH2Kreska = new double[lbIteracji+1, n];
             double[,] tabH2Daszek = new double[lbIteracji + 1, n];
             int[] tabTx = new int[n]; /*tablica, w której zapisane są czasy tx topnienia dla danego punktu x*/ 
-            double deltaX = b / n,deltaT=tGwiazdka/lbIteracji;
+            double deltaX = b / n,deltaT=0.1;
             double suma=0;
            
 
@@ -53,6 +56,8 @@ namespace ConsoleApp1
             for (int i = 0; i < n; i++)
                 tabTx[i] = lbIteracji; /* ustawiamy czas topnienia dla punktu x na lbIteracji */
 
+            
+
             for (int i = 0; i < temp0.Length; i++)
             {
                 if (temp0[i] > tempKrzep)
@@ -63,6 +68,7 @@ namespace ConsoleApp1
                     tabH[0, i] = cS * roS * temp0[i];
                 }
 
+            //Console.Write("tabH0[n-1]: {0}", tabH[0, 0]);
 
             /*Console.Write("tabH0: ");
             for (int i = 0; i < n; i++)
@@ -86,6 +92,8 @@ namespace ConsoleApp1
                 //Console.Write("tabHKreska: ");
                 //for (int i = 0; i < n; i++)
                 //    Console.Write("{0} ", tabHKreska[j-1, i]);
+
+                //Console.Write("tabHKreska[n-1]: {0}", tabHKreska[j-1, 0]);
 
                 aVec[0] = 0;
                 for (int i = 1; i < n - 1; i++)
@@ -122,6 +130,8 @@ namespace ConsoleApp1
                 for (int i = 0; i < n; i++)
                     tabHDaszek[j, i] = wynikThomas[i];
 
+               //Console.Write("tabHDaszek[n-1]: {0}", tabHDaszek[j, 0]);
+
                 //Console.WriteLine(" ");
                 //Console.Write("tabHDaszek wyznaczone z Thomasa: ");
                 //for (int i = 0; i < n; i++)
@@ -131,16 +141,22 @@ namespace ConsoleApp1
                 for (int i = 0; i < n; i++)
                     tabHKreska[j,i]=tabHDaszek[j, i]+(tabH[j-1,i]-tabHKreska[j-1,i]); /* korekta entalpii */
 
-               /* Console.WriteLine(" ");
-                Console.Write("tabHKreska po korekcie: ");
-                for (int i = 0; i < n; i++)
-                    Console.Write("{0} ", tabHKreska[j, i]); */
+                //Console.Write("tabHKreska[n-1]: {0}", tabHKreska[j, 0]);
+
+                /* Console.WriteLine(" ");
+                 Console.Write("tabHKreska po korekcie: ");
+                 for (int i = 0; i < n; i++)
+                     Console.Write("{0} ", tabHKreska[j, i]); */
 
 
                 a = lambdaS / (cS * roS); /* Etap 2b: sprowadzamy całość do fazy stałej  */
 
                 for (int i = 0; i < n; i++)
-                    tabH2Kreska[j-1, i] = Math.Min(tabHDaszek[j, i], entalpiaS);
+                {
+                    tabH2Kreska[j - 1, i] = Math.Min(tabHKreska[j, i], entalpiaS);
+                }
+
+                //Console.Write("tabH2Kreska[n-1]: {0}", tabH2Kreska[j-1, 0]);
 
                 //Console.WriteLine(" ");
                 //Console.Write("tabH2Kreska (minimum): ");
@@ -183,13 +199,17 @@ namespace ConsoleApp1
                 for (int i = 0; i < n; i++)
                     tabH2Daszek[j , i] = wynikThomas[i];
 
+                //Console.Write("tabH2Daszek[n-1]: {0}", tabH2Daszek[j, 0]);
+
                 //Console.WriteLine(" ");
-               // Console.Write("tabH2Daszek z thomasa: ");
-               // for (int i = 0; i < n; i++)
-                   // Console.Write("{0} ", tabH2Daszek[j, i]);
+                // Console.Write("tabH2Daszek z thomasa: ");
+                // for (int i = 0; i < n; i++)
+                // Console.Write("{0} ", tabH2Daszek[j, i]);
 
                 for (int i = 0; i < n; i++)
                     tabH[j, i] = tabH2Daszek[j, i] + (tabHKreska[j, i] - tabH2Kreska[j-1, i]); /* korekta */
+
+                //Console.Write("tabH[n-1]: {0}", tabH[j, 0]);
 
                 //Console.WriteLine(" ");
                 //Console.Write("tabH po korekcie: ");
@@ -207,7 +227,7 @@ namespace ConsoleApp1
 
                 //Console.WriteLine(" ");
 
-                for(int i=0;i<n;i++)
+                for (int i=0;i<n;i++)
                 {
                     if(tabH[j,i]>entalpiaS&&tabH[j-1,i]<=entalpiaS)
                     {
@@ -234,7 +254,7 @@ namespace ConsoleApp1
             {
                 
                 Console.Write("{0} ", tabTx[i]);
-            }
+            } 
              
             Console.WriteLine("");
 
