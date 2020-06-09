ce5365a6d1163478b482b4a58d72b8004870a6af
Agata <calkapotrojkacie@gmail.com>
2020-06-09 12:24:05 +0200

    Added exporting tabH table and temperatury table to csv files

--- Program.cs
+++ Program.cs
@@ -8,12 +8,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double cS=690, cL=800, roS=7500, roL=7000, L=270000, tempKrzep=1773, u0=1813,uI=323,lambdaL=104,lambdaS=240;
-            int lbIteracji = 300;
+            double cS=690, cL=800, roS=7500, roL=7000, L=270000, tempKrzep=1773, u0=2213,uI=323,lambdaL=104,lambdaS=240;
+            int lbIteracji = 5000;
             double a,stalaA,stalaSigma;
             double entalpiaL = 11200275000, entalpiaS = 9175275000;
             double b = 0.08,tGwiazdka=100;
             double alfa=0.5;
+            int granica = 0;
             
 
             //double[] temp0= { 50,50,50,50,50}; 
@@ -28,7 +29,7 @@ namespace ConsoleApp1
                 //temp0[i] = tempKrzep-i*dT;
                 temp0[i] = uI;
             }
-            temp0[0] = tempKrzep;
+            
             double[] aVec = new double[n];
             double[] bVec = new double[n];
             double[] cVec = new double[n];
@@ -41,7 +42,10 @@ namespace ConsoleApp1
             int[] tabTx = new int[n]; /*tablica, w której zapisane są czasy tx topnienia dla danego punktu x*/ 
             double deltaX = b / n,deltaT=0.1;
             double suma=0;
-           
+            int j = 1;
+            int l = 5;
+            double[] temperatury = new double[n];
+
 
             funkcje.MetodaThomasa thomas = new funkcje.MetodaThomasa(); /* obiekt klasy MetodaThomasa do rozwiązania układu równań */
             double[] wynikThomas;
@@ -77,9 +81,10 @@ namespace ConsoleApp1
 
             stalaSigma = 1 / (SpecialFunction.gamma(0.5) * (1 - alfa) * Math.Pow(deltaT, alfa)); /* wartość funkcji sigma */
 
-            
 
-            for (int j = 1; j < lbIteracji; j++)
+
+            //for (int j = 1; j < lbIteracji; j++)
+            while (granica < l&&j<lbIteracji)
             {
                 a = lambdaL / (cL*roL); /* Etap 2a: sprowadzamy całość do fazy ciekłej */
                 stalaA = -a / (Math.Pow(deltaX, 2));
@@ -235,20 +240,53 @@ namespace ConsoleApp1
                     }
 
                 }
+
+                for (int i=1;i<n-1;i++)
+                {
+                    if(tabH[j,i]<=entalpiaS&&tabH[j,i+1]<=entalpiaS&&tabH[j,i-1]>entalpiaS)
+                    {
+                        granica = i;
+                    }
+                }
+
+
+                Console.WriteLine("{0}", j);
+                j++;
             }
 
             Console.Write("tabH: ");
             for (int i = 0; i < n; i++)
             {
-                Console.Write("{0} ", tabH[lbIteracji-1, i]);
+                Console.Write("{0} ", tabH[j-1, i]);
             }
 
+            string path = @"C:\Users\calka\OneDrive\Pulpit\Grant\C#\ConsoleApp1\entalpie.csv";
+            System.IO.StreamWriter str = new System.IO.StreamWriter(path, false);
+
+            for (int i = 0; i < n; i++)
+            {
+                str.WriteLine("{0} ", tabH[j-1,i]);
+            }
+            str.Close();
+
+            Console.WriteLine("");
             Console.Write("temp tabH: ");
             for (int i = 0; i < n; i++)
             {
-                Console.Write("{0} ", tempZEntalpii.temperatura(tabH[lbIteracji - 1, i], tempKrzep, cL, roL, cS, roS, L, entalpiaL, entalpiaS));
+                temperatury[i] = tempZEntalpii.temperatura(tabH[j - 1, i], tempKrzep, cL, roL, cS, roS, L, entalpiaL, entalpiaS);
+                Console.Write("{0} ", temperatury[i]);
             }
 
+            path = @"C:\Users\calka\OneDrive\Pulpit\Grant\C#\ConsoleApp1\temperatury.csv";
+            str = new System.IO.StreamWriter(path, false);
+
+            for (int i = 0; i < n; i++)
+            {
+                str.WriteLine("{0} ", temperatury[i]);
+            }
+            str.Close();
+
+            Console.WriteLine("");
             Console.Write("Wartość tabTx:");
             for(int i=0;i<n;i++)
             {
@@ -257,6 +295,7 @@ namespace ConsoleApp1
             } 
              
             Console.WriteLine("");
+            Console.WriteLine("Program zakończył działanie po {0} iteracjach", j-1);
 
             temp = tempZEntalpii.temperatura(entalpiaL, tempKrzep, cL, roL, cS, roS, L, entalpiaL, entalpiaS);
             Console.WriteLine("Wartosc temperatury: {0}",temp);
@@ -265,6 +304,9 @@ namespace ConsoleApp1
             Console.WriteLine("Wartość g: {0} ",wynikG);
 
             Console.WriteLine("Wartość funkcji gamma: {0} ", SpecialFunction.gamma(0.5));
+
+
+
 
 
             Console.ReadKey();
